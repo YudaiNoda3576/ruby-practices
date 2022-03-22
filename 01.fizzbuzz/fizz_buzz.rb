179006720f1c5129a27f9c751cd82245c19ae2fa
YudaiNoda3576 <wind.d.r.3576@gmail.com>
2022-03-22 16:38:28 +0900

    refactor
    initializeの削除、クラスの外でメソッドを呼び出すように修正、出力する文字列を定数から除外、convert_fizzbuzzを実行するメソッドの名前をrunに変更

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -2,33 +2,22 @@ class FizzBuzz
   FIZZ_NUM = 3
   BUZZ_NUM = 5
 
-  TEXT_MULTIPLE_3 = 'Fizz'
-  TEXT_MULTIPLE_5 = 'Buzz'
-  TEXT_MULTIPLE_3_AND_5 = 'FizzBuzz'
-
-  def initialize(min, max)
-    @max = max
-    @min = min
-  end
-
-  def call_convert_fizzbuzz
-    @min.upto(@max) do |number|
+  def run(min, max)
+    min.upto(max) do |number|
       puts convert_fizzbuzz(number)
     end
   end
 
   def convert_fizzbuzz(number)
     if (number % FIZZ_NUM).zero? && number % BUZZ_NUM != 0
-      puts TEXT_MULTIPLE_3
+      puts 'Fizz'
     elsif number % FIZZ_NUM != 0 && (number % BUZZ_NUM).zero?
-      puts TEXT_MULTIPLE_5
+      puts 'Buzz'
     elsif (number % FIZZ_NUM).zero? && (number % BUZZ_NUM).zero?
-      puts TEXT_MULTIPLE_3_AND_5
+      puts 'FizzBuzz'
     else
-
       puts number
     end
   end
-
-  FizzBuzz.new(1, 20).call_convert_fizzbuzz
 end
+  FizzBuzz.new.run(1,20)
